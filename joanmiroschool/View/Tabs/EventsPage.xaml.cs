27414dab8d9b47fa3f5f98f34c68ee74c6334db0
Gatoreno <erp.reno@hotmail.com>
2020-12-03 20:47:16 -0500

    adding empty views

---
+++ joanmiroschool/View/Tabs/EventsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace joanmiroschool.View.Tabs
+{
+    public partial class EventsPage : ContentPage
+    {
+        public EventsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ joanmiroschool/View/Tabs/StudentsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace joanmiroschool.View.Tabs
+{
+    public partial class StudentsPage : ContentPage
+    {
+        public StudentsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
