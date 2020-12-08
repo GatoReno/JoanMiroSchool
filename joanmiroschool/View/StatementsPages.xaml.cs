ef2780b5d45e227d65340435599ae94ef1cd9fd2
Gatoreno <erp.reno@hotmail.com>
2020-12-08 00:19:45 -0500

    adding themes & missing bottom tab

---
+++ joanmiroschool/View/StatementsPages.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace joanmiroschool.View
+{
+    public partial class StatementsPages : ContentPage
+    {
+        public StatementsPages()
+        {
+            InitializeComponent();
+        }
+    }
+}
