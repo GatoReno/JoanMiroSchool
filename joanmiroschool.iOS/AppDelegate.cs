912cdb787f4b85753088da9aabfbdc50b741fae3
Gatoreno <erp.reno@hotmail.com>
2021-01-04 21:09:37 -0500

    adding ios push notifications

--- joanmiroschool.iOS/AppDelegate.cs
+++ joanmiroschool.iOS/AppDelegate.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 using Foundation;
 using UIKit;
+using UserNotifications;
+using WindowsAzure.Messaging;
 
 namespace joanmiroschool.iOS
 {
@@ -27,7 +30,108 @@ namespace joanmiroschool.iOS
             Firebase.Core.App.Configure();
             LoadApplication(new App());
 
-            return base.FinishedLaunching(app, options);
+            bool result = base.FinishedLaunching(app, options);
+
+            RegisteredForRemoteNotifications();
+
+            return result;
+        }
+
+        private void RegisteredForRemoteNotifications()
+        {
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                UNUserNotificationCenter.Current.RequestAuthorization
+                    (
+                        UNAuthorizationOptions.Alert |
+                        UNAuthorizationOptions.Badge |
+                        UNAuthorizationOptions.Sound,
+                        (granted, err) =>
+                        {
+                            InvokeOnMainThread(UIApplication.SharedApplication.RegisterForRemoteNotifications);
+                        }
+                    );
+            }
+            else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var pushSetting = UIUserNotificationSettings.GetSettingsForTypes(
+                    UIUserNotificationType.Alert | UIUserNotificationType.Badge |
+                    UIUserNotificationType.Sound, new NSSet()
+                    );
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSetting);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications();
+            }
+            else
+            {
+                UIRemoteNotificationType notificationType =  
+                    UIRemoteNotificationType.Alert |
+                    UIRemoteNotificationType.Sound |
+                    UIRemoteNotificationType.Badge;
+                UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationType);
+            }
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            //base.RegisteredForRemoteNotifications(application, deviceToken);
+            var hub = new SBNotificationHub(
+                "Endpoint=sb://jmsresourcesh.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=VFtnJKJoa+J6D/C5f7/DYllC1ONcgvk47OuwvHPmOMw=",
+                "JMSNotificationHub");
+            hub.UnregisterAll(deviceToken,(err)=>
+            {
+                if (err != null)
+                {
+                    return;
+                }
+
+                var tags = new NSSet((new string[] { "default" }).ToArray());
+                hub.RegisterNative(deviceToken, tags,
+                    (err2) =>
+                    {
+                        Debug.WriteLine($"{err2}");
+                    });
+
+
+                var tempoExp = DateTime.Now.AddDays(100).
+                    ToString(System.Globalization.CultureInfo.
+                    CreateSpecificCulture("en-US"));
+
+
+                hub.RegisterTemplate(deviceToken, "defaultTemplate",
+                    "{\"aps\":{\"alert\":\"Notification Hub test notification\"}}",
+                    tempoExp, tags,
+                    (err3) =>
+                    {
+                        Debug.WriteLine($"{err3}");
+                    });
+            });
+
+        }
+
+        public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+        {
+            ProcessNotification(userInfo);
+        }
+
+        private void ProcessNotification(NSDictionary options)
+        {
+            if (options != null && options.ContainsKey(new NSString("aps")))
+            {
+                NSDictionary aps = options.ObjectForKey(new NSString("aps")) as NSDictionary;
+                string pl = string.Empty;
+                if (aps.ContainsKey(new NSString("alert")))
+                {
+                    pl = aps[new NSString("alert")].ToString();
+                }
+                if (!string.IsNullOrEmpty(pl))
+                {
+                    // (App.Current.MainPage as MainPage)
+                }
+            }
+            else
+            {
+                Debug.WriteLine($"{options}");
+            }
         }
     }
 }
